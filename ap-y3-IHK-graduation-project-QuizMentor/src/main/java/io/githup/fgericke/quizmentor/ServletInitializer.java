30b2057016c9232a2813b033c5ab99de725db1b3
fGericke <florian.gericke@endava.com>
2024-05-07 14:36:01 +0200

    setUp Basic

---
+++ ap-y3-IHK-graduation-project-QuizMentor/src/main/java/io/githup/fgericke/quizmentor/ServletInitializer.java
@@ -0,0 +1,15 @@
+package io.githup.fgericke.quizmentor;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder application) {
+    return application.sources(
+        QuizMentor.class);
+  }
+
+}
