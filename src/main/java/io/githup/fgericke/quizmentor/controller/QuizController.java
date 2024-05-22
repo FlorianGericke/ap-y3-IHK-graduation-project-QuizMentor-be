b246a075a40f975c706ba73c19afd4e932999f6b
fGericke <florian.gericke@endava.com>
2024-05-22 11:32:22 +0200

    Add Swagger Annotation Dependency to Configure and Describe Swagger Ui

--- src/main/java/io/githup/fgericke/quizmentor/controller/QuizController.java
+++ src/main/java/io/githup/fgericke/quizmentor/controller/QuizController.java
@@ -6,12 +6,14 @@ import io.githup.fgericke.quizmentor.dto.response.QuizResponse;
 import io.githup.fgericke.quizmentor.entity.Quiz;
 import io.githup.fgericke.quizmentor.repository.QuizRepository;
 import io.githup.fgericke.quizmentor.service.QuizService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/v1/quiz")
+@Tag(name = "Quiz", description = "The Quiz API")
 public class QuizController extends BaseController<
     Quiz,
     QuizRepository,
