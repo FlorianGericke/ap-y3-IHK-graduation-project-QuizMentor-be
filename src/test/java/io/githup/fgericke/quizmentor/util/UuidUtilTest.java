2e20a2df2b831762460601fddc5e5b237120bb36
fGericke <florian.gericke@endava.com>
2024-06-19 16:29:30 +0200

    fixing problem that the iri gets generated wrong from en entity

--- src/test/java/io/githup/fgericke/quizmentor/util/UuidUtilTest.java
+++ src/test/java/io/githup/fgericke/quizmentor/util/UuidUtilTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 import io.githup.fgericke.quizmentor.entity.generic.BaseEntity;
 import java.util.UUID;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,46 +13,11 @@ import org.junit.jupiter.api.Test;
 class UuidUtilTest {
 
   /**
-   * Test to verify that getIri method returns null when the provided UUID is null.
-   */
-  @Test
-  void getIriReturnsNullWhenUuidIsNull() {
-    assertNull(UuidUtil.getIri((UUID) null));
-  }
-
-  /**
-   * Test to verify that getIri method returns the correct IRI when the provided UUID is not null.
-   */
-  @Test
-  void getIriReturnsIriWhenUuidIsNotNull() {
-    UUID uuid = UUID.randomUUID();
-    String expectedIri = EnvironmentUtil.getApiBaseUrl() + uuid;
-    assertEquals(expectedIri, UuidUtil.getIri(uuid));
-  }
-
-  /**
    * Test to verify that getIri method returns null when the provided BaseEntity is null.
    */
   @Test
   void getIriReturnsNullWhenEntityIsNull() {
-    assertNull(UuidUtil.getIri((BaseEntity) null));
-  }
-
-  /**
-   * Test to verify that getIri method returns the correct IRI when the provided BaseEntity is not
-   * null.
-   */
-  @Test
-  void getIriReturnsIriWhenEntityIsNotNull() {
-    UUID id = UUID.randomUUID();
-    BaseEntity entity = new BaseEntity() {
-      @Override
-      public UUID getId() {
-        return id;
-      }
-    };
-    String expectedIri = EnvironmentUtil.getApiBaseUrl() + entity.getId();
-    assertEquals(expectedIri, UuidUtil.getIri(entity));
+    assertNull(UuidUtil.getIri(null));
   }
 
   /**
@@ -72,16 +36,6 @@ class UuidUtilTest {
     assertNull(UuidUtil.getUuid(""));
   }
 
-  /**
-   * Test to verify that getUuid method returns the correct UUID when the provided IRI is valid.
-   */
-  @Test
-  @Disabled
-  void getUuidReturnsUuidWhenIriIsValid() {
-    UUID uuid = UUID.randomUUID();
-    String iri = EnvironmentUtil.getApiBaseUrl() + uuid;
-    assertEquals(uuid, UuidUtil.getUuid(iri));
-  }
 
   /**
    * Test to verify that getUuid method returns the correct UUID when the provided UUID string is
